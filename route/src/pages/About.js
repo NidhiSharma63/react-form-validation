3b174aca594848330f5636ce59f22b0a2cd69001
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-06-25 17:33:58 +0530

    navigate functionality added

--- route/src/pages/About.js
+++ route/src/pages/About.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
 
 export default function About() {
+  const navigate = useNavigate();
   return (
     <div>
       <h1>Hello from About Page 🧍 </h1>
+      <button onClick={()=>{
+        navigate('/')
+      }}>Home</button>
     </div>
   )
 }
--- route/src/pages/Contact.js
+++ route/src/pages/Contact.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+
 
 export default function Contact() {
+  const navigate = useNavigate();
   return (
     <>
+
       <h1>hello from contact page 😃</h1>
+      <button onClick={()=>{
+        navigate('/')
+      }}>Home</button>
     </>
   )
 }
